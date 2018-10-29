3bd801b9e8e8973677eef994b4273bcc84604b8f
arunmoezhi <arunmoezhi@gmail.com>
2018-10-29 13:03:04 -0700

    formatting changes only

--- knn.py
+++ knn.py
@@ -11,7 +11,7 @@ import sys
 
 def main():
   predictionService()
-  
+
 def predictionService():
   # Load dataset
   df = pandas.read_csv("dataset/test.csv")
@@ -26,25 +26,25 @@ def predictionService():
   # all values must be integers for modelling
   le = preprocessing.LabelEncoder()
   df["site"] = le.fit_transform(df["site"])
-  
+
   df = normalize(df) #optional normalization
   array = df.values
   X = array[:,0:numOfColumns-1] # all features
   Y = array[:,numOfColumns-1] # output
   validation_size = 0.01
-  
+
   X_train = X[0:int((1-validation_size)*numOfRows),:]
   X_validation = X[int((1-validation_size)*numOfRows):,:]
   Y_train = Y[0:int((1-validation_size)*numOfRows)]
   Y_validation = Y[int((1-validation_size)*numOfRows):]
   assert numOfRows == len(X_train) + len(X_validation)
   print("knn begin")
-  
+
   if(len(sys.argv) < 2):
     train_predict_knn(X_train,Y_train, X_validation, Y_validation)
     return
 
-  if(sys.argv[1] == "t"):  
+  if(sys.argv[1] == "t"):
     train_knn(X_train,Y_train, X_validation, Y_validation)
   elif(sys.argv[1] == "p"):
     predict_knn(X_validation, Y_validation)
@@ -59,7 +59,7 @@ def train_knn(X_train,Y_train, X_validation, Y_validation):
   print("knn training complete")
   pickle.dump(knnModel, open("knn.model", 'wb'))
   print("Training   time for %d samples : %d seconds (Rate %d samples/second)" % (Y_train.size, trainTime, Y_train.size/trainTime))
-  
+
 def predict_knn(X_validation, Y_validation):
   knnModel = pickle.load(open("knn.model", 'rb'))
   startTime = time.time()
@@ -68,7 +68,7 @@ def predict_knn(X_validation, Y_validation):
   print("Accuracy : %.2f %%" % (100*accuracy_score(Y_validation, predictions)))
   print("knn prediction done\n")
   print("Prediction time for %d samples : %d seconds (Rate %d samples/second)" % (Y_validation.size, predictionTime, Y_validation.size/predictionTime))
-  
+
 def train_predict_knn(X_train,Y_train, X_validation, Y_validation):
   startTime = time.time()
   knnModel = KNeighborsClassifier(n_neighbors=6, p=2, weights='distance', n_jobs=-1, algorithm='kd_tree',leaf_size=2)
@@ -83,7 +83,7 @@ def train_predict_knn(X_train,Y_train, X_validation, Y_validation):
   print("Training   time for %d samples : %d seconds (Rate %d samples/second)" % (Y_train.size, trainTime, Y_train.size/trainTime))
   print("Prediction time for %d samples : %d seconds (Rate %d samples/second)" % (Y_validation.size, predictionTime, Y_validation.size/predictionTime))
 
- 
+
 def normalize(df):
   dfOrig = df;
   df = df.drop(['workbook'], axis=1)
@@ -92,6 +92,6 @@ def normalize(df):
   df = pandas.DataFrame(arr_scaled, columns=list(df));
   df["workbook"] = dfOrig["workbook"]
   return df
-  
+
 if __name__ == '__main__':
   main()
